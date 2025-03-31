a8bcdea1b0e3f5200c6de82eed9016c78457ac2b
Madman10K <stanv.contact176@gmail.com>
2025-04-01 00:04:52 +0300

    Try deploy

--- run.sh
+++ run.sh
@@ -13,10 +13,23 @@ compile_file() {
 
 	sed -i "s/sha256sums=(.*/sha256sums=()/g" PKGBUILD
 	sha=$(makepkg -g)
-        sed -i "s/sha256sums=()/${sha}/g" PKGBUILD
+	sed -i "s/sha256sums=()/${sha}/g" PKGBUILD
  
 	makepkg -si --noconfirm || exit
 	makepkg --printsrcinfo > .SRCINFO || exit
+
+	if [ "$2" == "deploy" ];
+		git init
+		git remote add origin ssh://aur@aur.archlinux.org/"$1"
+		git fetch origin
+		
+		git add . && git commit -m "Automatic"
+		git checkout master
+		git diff master main | git apply --cached
+		git commit -m "Automatic update"
+		git push origin HEAD:master
+	fi
+
 	cd ../ || exit
 
 	echo -e "\x1b[32m--------------------------------\x1b[0m"
@@ -26,14 +39,14 @@ compile_file() {
 
 export -f compile_file
 
-compile_file untitled-i18n
-compile_file untitled-open
-compile_file untitled-xdg-basedir
-compile_file untitled-exec
-compile_file untitled-cli-parser
-compile_file untitled-dbus-utils
+compile_file untitled-i18n "$1"
+compile_file untitled-open "$1"
+compile_file untitled-xdg-basedir $1
+compile_file untitled-exec $1
+compile_file untitled-cli-parser $1
+compile_file untitled-dbus-utils $1
 
-compile_file untitled-imgui-framework
-compile_file ude-session-logout
-compile_file untitled-game-system-manager
-compile_file untitled-ibus-handwriting
+compile_file untitled-imgui-framework $1
+compile_file ude-session-logout $1
+compile_file untitled-game-system-manager $1
+compile_file untitled-ibus-handwriting $1
